2e85a68a174b45d5d34e19f7b58fe21fa9d2010c
Brian Quiroz <r229q057@ku.edu>
2020-02-18 16:12:56 -0600

    Started working on UI of settings page (#18)

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,58 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SettingsTitleBox } from '../components/SettingsTitleBox';
+import { SettingsOption } from '../components/SettingsOption';
 
-export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+const titleTexts = {
+  title1: 'My Account',
+  title2: 'App Support'
+}
+
+const optionTexts = {
+  option1: 'Account Details',
+  option2: 'Notifications',
+  option3: 'Edit Pills',
+  option4: 'Alexa',
+  option5: 'Dark Mode',
+  option6: 'Sign Out',
+  option7: 'Help',
+  option8: 'App Version'
+}
+
+export default class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <SettingsTitleBox titleText={titleTexts.title1} />
+          <SettingsOption optionText={optionTexts.option1} />
+          <SettingsOption optionText={optionTexts.option2} />
+          <SettingsOption optionText={optionTexts.option3} />
+          <SettingsOption optionText={optionTexts.option4} />
+          <SettingsOption optionText={optionTexts.option5} />
+          <SettingsOption optionText={optionTexts.option6} />
+          <SettingsTitleBox titleText={titleTexts.title2} />
+          <SettingsOption optionText={optionTexts.option7} />
+          <SettingsOption optionText={optionTexts.option8} />
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 SettingsScreen.navigationOptions = {
   title: 'Settings',
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  contentContainer: {
+    minHeight: '100%',
+  },
+});
